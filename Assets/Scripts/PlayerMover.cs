e53190a1d5758b5a041a09c52297956ae5144b81
David Corticchiato <david.corticchiato@gmail.com>
2020-07-04 11:51:29 -0400

    sample with skeleton moves

---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D body;
+
+    [SerializeField]
+    private float velocity;
+
+    void Update()
+    {
+        body.velocity = new Vector2(velocity * Math.Sign(Input.GetAxis("Horizontal")), velocity * Math.Sign(Input.GetAxis("Vertical")));
+    }
+}
