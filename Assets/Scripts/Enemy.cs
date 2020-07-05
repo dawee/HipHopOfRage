0629cf83be7efd20b881390711356560c3dad6db
David Corticchiato <david.corticchiato@gmail.com>
2020-07-05 10:48:15 -0400

    death animation

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator;
+
+    public void Hit(float damage)
+    {
+        animator.SetTrigger("death");
+    }
+}
