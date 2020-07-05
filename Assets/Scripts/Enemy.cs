8ca1b12d20d835d50248d0858813cb904d685061
David Corticchiato <david.corticchiato@gmail.com>
2020-07-05 11:03:51 -0400

    trigger an event on when dead

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,14 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField]
     private Animator animator;
 
+    [SerializeField]
+    private UnityEvent died;
+
     public void Hit(float damage)
     {
+        Die();
+    }
+
+    private void Die()
+    {
         animator.SetTrigger("death");
+        died.Invoke();
     }
 }
