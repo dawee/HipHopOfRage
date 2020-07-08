c6c58a755e21c8ea53177b64392a1a69a95fc00f
David Corticchiato <david.corticchiato@gmail.com>
2020-07-07 20:35:50 -0400

    stop enemy hit after death

--- Assets/Scripts/EnnemyHit.cs
+++ Assets/Scripts/EnnemyHit.cs
@@ -7,22 +7,24 @@ public class EnnemyHit : MonoBehaviour
 
     [SerializeField]
     private HitCollide hit;
-    
-    // Start is called before the first frame update
+
     void Start()
     {
         StartCoroutine(WaitForHit());
     }
 
-    IEnumerator WaitForHit() {
-        yield return new WaitForSeconds(2);
-        hit.Activate();
-        StartCoroutine(WaitForHit());
-    }
-
-    // Update is called once per frame
-    void Update()
+    IEnumerator WaitForHit()
     {
-        
+        yield return new WaitForSeconds(2);
+
+        if (enabled)
+        {
+            hit.Activate();
+            StartCoroutine(WaitForHit());
+        }
+        else
+        {
+            yield break;
+        }
     }
 }
