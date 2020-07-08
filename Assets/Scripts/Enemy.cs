51332ca37dd9161f89bf02e95789daf4381c29b5
David Corticchiato <david.corticchiato@gmail.com>
2020-07-08 18:41:06 -0400

    enemis following player

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -10,6 +11,36 @@ public class Enemy : MonoBehaviour
 
     [SerializeField]
     private Wave wave = default;
+
+    [SerializeField]
+    private PlayerMover player = default;
+
+    [SerializeField]
+    private float minFollowDistance = default;
+
+    [SerializeField]
+    private Rigidbody2D body = default;
+
+    [SerializeField]
+    private float velocity = default;
+
+    private static Vector3 rightScale = new Vector3(1, 1, 1);
+    private static Vector3 leftScale = new Vector3(-1, 1, 1);
+
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, player.transform.position) > minFollowDistance)
+        {
+            transform.localScale = player.transform.position.x > transform.position.x ? rightScale : leftScale;
+            body.velocity = (player.transform.position - transform.position).normalized * velocity;
+            animator.SetBool("walk", true);
+        }
+        else
+        {
+            body.velocity = Vector2.zero;
+            animator.SetBool("walk", false);
+        }
+    }
 
     public void Hit(float damage)
     {
