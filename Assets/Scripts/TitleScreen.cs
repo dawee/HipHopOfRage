15eee8c53f6d69653df293b0f941825ebb5e4cb8
Skaradams <skaradams69@gmail.com>
2020-07-11 17:08:49 +0200

    add title screen

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreen : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject game;
+    
+    [SerializeField]
+    private GameObject titleScreen;
+
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private bool isStarted = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        game.SetActive(true);
+        titleScreen.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Hit"))
+        {
+            animator.SetTrigger("start");
+        }
+        if(isStarted)
+        {
+            StartGame();
+        }
+    }
+}
