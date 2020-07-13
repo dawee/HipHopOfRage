2a4fa3be6b8720f004707d30188210ac462099c8
David Corticchiato <david.corticchiato@gmail.com>
2020-07-12 20:32:17 -0400

    switch to boss music when enters area

---
+++ Assets/Scripts/BossArea.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossArea : MonoBehaviour
+{
+
+    [SerializeField]
+    private Jukebox jukebox = default;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            jukebox.OnEnterBossArea();
+        }
+    }
+
+}
---
+++ Assets/Scripts/Jukebox.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Jukebox : MonoBehaviour
+{
+
+    [SerializeField]
+    private AudioSource levelMusic = default;
+
+    [SerializeField]
+    private AudioSource bossMusic = default;
+
+    public void OnEnterBossArea()
+    {
+        levelMusic.Stop();
+        bossMusic.Play();
+    }
+
+    void Start()
+    {
+        bossMusic.Stop();
+        levelMusic.Play();
+    }
+}
